658f80fce967a5df92f30eb8a9ba299c28c84ffb
Clatson <124655279+clatsonin@users.noreply.github.com>
2025-01-23 23:50:18 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['i.seadn.io','img.reservoir.tools','i.nfte.ai','lh3.googleusercontent.com','randomuser.me'], 
+      },
+};
+
+export default nextConfig;
